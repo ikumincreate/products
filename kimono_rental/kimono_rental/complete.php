23d828a6e9fac443bbce8d2e02010d43010b11a7
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-11-28 15:41:10 +0900

    WP化前のファイル

--- kimono_rental/kimono_rental/complete.php
+++ kimono_rental/kimono_rental/complete.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="京都にある着物レンタルサービス店のサイトです。" />
   <title>着物レンタル 小島</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="style.css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -56,12 +56,12 @@
   </header>
 
   <main>
-  <h2>ご予約を承りました</h2>
+  <h2>お申込みを承りました</h2>
     <p>
       ありがとうございます。<br>
       ご予約が完了しましたら、メールにてお知らせいたします。<br>
     </p>
-    <button id="back-btn" onclick="location.href='index.php';">戻る</button>
+    <button id="back-btn" onclick="location.href='index.php';" class="btn">TOPに戻る</button>
 
   </main>
 </body>
@@ -69,19 +69,25 @@
 <footer>
     <!-- アクセス -->
     <section id="access">
-      <div id="info">
-        <p id="shop_name">着物レンタル　小島</p>
-        <p class="shop_info">住所：〒600-8216 京都府京都市下京区塩小路通不明門東入東塩小路町７１７−１</p>
-        <p class="shop_info">京都駅中央口から徒歩5分</p>
-        <p class="shop_info">電話：000-000-0000</p>
-        <p class="shop_info">営業時間：9:00～20:00</p>
-        <p class="shop_info">instagram：@kimonokojima</p>
+    <div id="container">
+      <div class="inner">
+        <div id="info">
+          <p id="shop_name">着物レンタル　小島</p>
+          <p class="shop_info">住所：〒600-8216<br> 京都府京都市下京区塩小路通不明門東入東塩小路町７１７−１</p>
+          <p class="shop_info">京都駅中央口から徒歩5分</p>
+          <p class="shop_info">電話：000-000-0000</p>
+          <p class="shop_info">営業時間：9:00～20:00</p>
+          <p class="shop_info">Instagram：@kimonokojima</p>
+        </div>
+      </div>    
+      <div class="inner">
+        <div id="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7717740574244!2d135.75792757575695!3d34.98738097281916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108ae35cc5255%3A0x2d926c53f55b49b2!2z44CSNjAwLTgyMTYg5Lqs6YO95bqc5Lqs6YO95biC5LiL5Lqs5Yy65aGp5bCP6Lev6YCa5LiN5piO6ZaA5p2x5YWl5p2x5aGp5bCP6Lev55S677yX77yR77yX4oiS77yRIOS6rOmDvemnheWJjeODk-ODqw!5e0!3m2!1sja!2sjp!4v1700555449564!5m2!1sja!2sjp" 
+          width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
       </div>
-      <div id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7717740574244!2d135.75792757575695!3d34.98738097281916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108ae35cc5255%3A0x2d926c53f55b49b2!2z44CSNjAwLTgyMTYg5Lqs6YO95bqc5Lqs6YO95biC5LiL5Lqs5Yy65aGp5bCP6Lev6YCa5LiN5piO6ZaA5p2x5YWl5p2x5aGp5bCP6Lev55S677yX77yR77yX4oiS77yRIOS6rOmDvemnheWJjeODk-ODqw!5e0!3m2!1sja!2sjp!4v1700555449564!5m2!1sja!2sjp" 
-        width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
-      </div>
+    </div>
     </section>
     <p id="copy">&copy; kimonorental KOJIMA. All rights reserved.</p>
   </footer>
--- kimono_rental/kimono_rental/index.php
+++ kimono_rental/kimono_rental/index.php
@@ -6,7 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="京都にある着物レンタルサービス店のサイトです。" />
   <title>着物レンタル 小島</title>
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+  <link rel="stylesheet" href="style.css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,25 +16,21 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Kaisei+Decol&display=swap" rel="stylesheet">
-
 </head>
 
 <body>
-  <!-- ナビゲーションメニュー -->
   <header>
+  <!-- ナビゲーションメニュー -->
   <!-- PC用ナビゲーションメニュー -->
-    <div class="innner">
-      <a id="logo" href="index.html"><img src="img/logo.png" alt="トップページに戻る"></a>
-    </div>
-    <div class="innner">
-      <nav id="nav_pc">
-        <a href="index.php#rental_plan">レンタルプラン</a>
-        <a href="index.php#rental_flow">レンタルの流れ</a>
-        <a href="index.php#reservation">ご予約</a>
-        <a href="index.php#gallery">ギャラリー</a>
-        <a href="index.php#access">アクセス</a>
-      </nav>
-  </div>
+  <a id="logo" href="index.php"><img src="img/logo.png" alt="トップページに戻る"></a>
+  <nav id="nav_pc">
+    <a href="index.php#rental_plan">レンタルプラン</a>
+    <a href="index.php#rental_flow">レンタルの流れ</a>
+    <a href="index.php#reservation">ご予約</a>
+    <a href="index.php#gallery">ギャラリー</a>
+    <a href="index.php#access">アクセス</a>
+  </nav>
+
 
   <!-- スマホ用ナビゲーションメニュー -->
   <img id="menu_sp" src="img/button-menu.png" alt="ナビゲーションを開く" onclick="document.getElementById('nav_sp').style.display = 'block'">
@@ -55,11 +52,11 @@
   </nav>
   </header>
 
+  <!-- メイン画像 -->
+  <img src="img/main4.png" alt="着物を着た女性の後ろ姿">
+
   <main>
-    <!-- メイン画像 -->
-    <section id="main_visual">
-      <img src="img/main4.png" alt="着物を着た女性の後ろ姿">
-    </section>
+
 
     <h1 id="main_text">
       古典的な和柄から、華やかなモダン柄まで豊富な着物を取り揃えていますので<br>
@@ -286,7 +283,7 @@
         </div>
         </div>
       <div id="insta">
-        <a id="insta_link" href="https://www.instagram.com/">instagramでもっと見る</a>
+        <a id="link" href="https://www.instagram.com/">Instagramでもっと見る</a>
       </div>
     </section>
 
@@ -295,19 +292,25 @@
   <footer>
     <!-- アクセス -->
     <section id="access">
-      <div id="info">
-        <p id="shop_name">着物レンタル　小島</p>
-        <p class="shop_info">住所：〒600-8216 京都府京都市下京区塩小路通不明門東入東塩小路町７１７−１</p>
-        <p class="shop_info">京都駅中央口から徒歩5分</p>
-        <p class="shop_info">電話：000-000-0000</p>
-        <p class="shop_info">営業時間：9:00～20:00</p>
-        <p class="shop_info">instagram：@kimonokojima</p>
+    <div id="container">
+      <div class="inner">
+        <div id="info">
+          <p id="shop_name">着物レンタル　小島</p>
+          <p class="shop_info">住所：〒600-8216<br> 京都府京都市下京区塩小路通不明門東入東塩小路町７１７−１</p>
+          <p class="shop_info">京都駅中央口から徒歩5分</p>
+          <p class="shop_info">電話：000-000-0000</p>
+          <p class="shop_info">営業時間：9:00～20:00</p>
+          <p class="shop_info">Instagram：@kimonokojima</p>
+        </div>
+      </div>    
+      <div class="inner">
+        <div id="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7717740574244!2d135.75792757575695!3d34.98738097281916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108ae35cc5255%3A0x2d926c53f55b49b2!2z44CSNjAwLTgyMTYg5Lqs6YO95bqc5Lqs6YO95biC5LiL5Lqs5Yy65aGp5bCP6Lev6YCa5LiN5piO6ZaA5p2x5YWl5p2x5aGp5bCP6Lev55S677yX77yR77yX4oiS77yRIOS6rOmDvemnheWJjeODk-ODqw!5e0!3m2!1sja!2sjp!4v1700555449564!5m2!1sja!2sjp" 
+          width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+          </iframe>
+        </div>
       </div>
-      <div id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.7717740574244!2d135.75792757575695!3d34.98738097281916!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x600108ae35cc5255%3A0x2d926c53f55b49b2!2z44CSNjAwLTgyMTYg5Lqs6YO95bqc5Lqs6YO95biC5LiL5Lqs5Yy65aGp5bCP6Lev6YCa5LiN5piO6ZaA5p2x5YWl5p2x5aGp5bCP6Lev55S677yX77yR77yX4oiS77yRIOS6rOmDvemnheWJjeODk-ODqw!5e0!3m2!1sja!2sjp!4v1700555449564!5m2!1sja!2sjp" 
-        width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
-      </div>
+    </div>
     </section>
     <p id="copy">&copy; kimonorental KOJIMA. All rights reserved.</p>
   </footer>
