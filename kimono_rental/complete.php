47fe2a858d37c634ef0497466f8bfe6a215f257e
ikumincreate <141818014+ikumincreate@users.noreply.github.com>
2023-12-07 11:24:10 +0900

    予約フォーム編集、プランページ作成中

---
+++ kimono_rental/complete.php
@@ -0,0 +1,14 @@
+<?php get_header();?>
+
+  <main>
+  <h2>お申込みを承りました</h2>
+    <p>
+      ありがとうございます。<br>
+      ご予約が完了しましたら、メールにてお知らせいたします。<br>
+    </p>
+    <button id="back-btn" onclick="location.href='index.php';" class="btn">TOPに戻る</button>
+
+  </main>
+</body>
+
+<?php get_footer(); ?>
---
+++ kimono_rental/single.php
@@ -0,0 +1,14 @@
+
+
+<!-- header読み込み -->
+<?php get_header();?>
+
+
+<!-- <h1><?php the_title(); ?></h1> -->
+
+
+
+<?php the_content(); ?>
+
+<!-- footer読み込み -->
+<?php get_footer(); ?>
